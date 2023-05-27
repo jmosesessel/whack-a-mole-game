c30d583328592bec3e0b0b8731a74aa5f41be865
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-05-27 15:33:34 +0000

    complete game with sound on whack

---
+++ script copy.js
@@ -0,0 +1,106 @@
+let initialTimerSecs = 10;
+let initialScore = 0;
+let selectedHole = 0;
+let countDownTimer;
+
+// add event to the start button
+const startBtn = document
+	.querySelector(".start-game button")
+	.addEventListener("click", (event) => {
+		console.log("starting...");
+
+        
+
+		//showMole("hole3");
+		resetAll();
+		countDown(initialTimerSecs);
+        generateRandomAppearance()
+        chooseRandomHole(1,6)
+	});
+
+const countDown = (seconds) => {
+	clearInterval(countDownTimer);
+
+	countDownTimer = setInterval(() => {
+		const newTime = seconds--;
+		console.log("count down", newTime);
+		displayCountDown(newTime);
+		if (newTime == 0) {
+			clearInterval(countDownTimer);
+			console.log("time up...");
+            hideMoles();
+		}
+	}, 1000);
+};
+
+// display countDown
+const displayCountDown = (timeLeft) => {
+	document.querySelector(".timer").textContent = timeLeft;
+};
+
+// display score
+const displayScore = (score) => {
+	document.querySelector(".score").textContent = score;
+};
+
+// show mole
+const showMole = (hole) => {
+	const mole = document.querySelector(`.${hole} .mole`);
+	mole.style.top = "0";
+};
+
+// hide mole
+const hideMoles = () => {
+	const moles = document.querySelectorAll(`.mole`).forEach(mole=>{
+        mole.style.top = "100%";
+    });
+};
+
+// whacked mole. add click event to mole
+function whackMole(event) {
+	updateScore(initialScore);
+	const selectedHole = event.srcElement.parentElement.className
+		.toString()
+		.split(" ")[1];
+	console.log("whacked...", selectedHole, event);
+	hideMoles();
+    chooseRandomHole(1,6)
+}
+
+const clickedMole = document.querySelectorAll(".mole").forEach((mole) => {
+	mole.addEventListener("click", (event) => whackMole(event));
+});
+
+// update score
+const updateScore = (score) => {
+	initialScore = score+1;
+	console.log("initial score", initialScore);
+	displayScore(initialScore);
+};
+
+// generate random number between 0 and 1
+const generateRandomAppearance = ()=>{
+    const random = Math.random(0,1)
+    console.log('random Appearance', Math.round(random))
+}
+
+const chooseRandomHole = (min,max)=>{
+    const random = Math.floor(Math.random() * (max - min + 1) + min)
+    //show a mole in the selected hole
+    const hole = `hole${random}`
+    selectedHole = hole
+    showMole(selectedHole)
+    console.log('random hole', hole)
+}
+
+const resetAll = () => {
+	//reset timer
+	displayCountDown(initialTimerSecs);
+
+    //hide moles
+    hideMoles()
+	// reset score
+    initialScore = 0;
+	displayScore(initialScore);
+
+};
--- script.js
+++ script.js
@@ -1,106 +1,69 @@
-let initialTimerSecs = 10;
-let initialScore = 0;
-let selectedHole = 0;
-let countDownTimer;
-
-// add event to the start button
-const startBtn = document
-	.querySelector(".start-game button")
-	.addEventListener("click", (event) => {
-		console.log("starting...");
-
-        
-
-		//showMole("hole3");
-		resetAll();
-		countDown(initialTimerSecs);
-        generateRandomAppearance()
-        chooseRandomHole(1,6)
-	});
-
-const countDown = (seconds) => {
-	clearInterval(countDownTimer);
-
-	countDownTimer = setInterval(() => {
-		const newTime = seconds--;
-		console.log("count down", newTime);
-		displayCountDown(newTime);
-		if (newTime == 0) {
-			clearInterval(countDownTimer);
-			console.log("time up...");
-            hideMoles();
+let timer = document.querySelector(".timer");
+let score = document.querySelector(".score");
+let button = document.querySelector("button");
+let holes = document.querySelectorAll(".hole");
+let moles = document.querySelectorAll(".mole");
+let lastIndex;
+let countDown_timer;
+let popUp_timer;
+let timeOver = false;
+let gameTimer;
+function countDown() {
+	countDown_timer = setInterval(() => {
+		if (timer.textContent == 0) {
+			clearInterval(countDown_timer);
+			return;
 		}
+		timer.textContent--;
 	}, 1000);
-};
-
-// display countDown
-const displayCountDown = (timeLeft) => {
-	document.querySelector(".timer").textContent = timeLeft;
-};
-
-// display score
-const displayScore = (score) => {
-	document.querySelector(".score").textContent = score;
-};
-
-// show mole
-const showMole = (hole) => {
-	const mole = document.querySelector(`.${hole} .mole`);
-	mole.style.top = "0";
-};
-
-// hide mole
-const hideMoles = () => {
-	const moles = document.querySelectorAll(`.mole`).forEach(mole=>{
-        mole.style.top = "100%";
-    });
-};
-
-// whacked mole. add click event to mole
-function whackMole(event) {
-	updateScore(initialScore);
-	const selectedHole = event.srcElement.parentElement.className
-		.toString()
-		.split(" ")[1];
-	console.log("whacked...", selectedHole, event);
-	hideMoles();
-    chooseRandomHole(1,6)
 }
-
-const clickedMole = document.querySelectorAll(".mole").forEach((mole) => {
-	mole.addEventListener("click", (event) => whackMole(event));
-});
-
-// update score
-const updateScore = (score) => {
-	initialScore = score+1;
-	console.log("initial score", initialScore);
-	displayScore(initialScore);
-};
-
-// generate random number between 0 and 1
-const generateRandomAppearance = ()=>{
-    const random = Math.random(0,1)
-    console.log('random Apperance', Math.round(random))
+function popUp() {
+	let holeIndex = randomPopUp();
+	const randomtime = randomTimer(2000, 500);
+	holes[holeIndex].classList.add("up");
+	popUp_timer = setTimeout(() => {
+		holes[holeIndex].classList.remove("up");
+		if (timeOver == false) {
+			popUp();
+		}
+		// if(!timeOver) popUp();
+	}, randomtime);
 }
-
-const chooseRandomHole = (min,max)=>{
-    const random = Math.floor(Math.random() * (max - min + 1) + min)
-    //show a mole in the selected hole
-    const hole = `hole${random}`
-    selectedHole = hole
-    showMole(selectedHole)
-    console.log('random hole', hole)
+function randomPopUp() {
+	let index = Math.floor(Math.random() * holes.length);
+	if (lastIndex == index) {
+		return randomPopUp();
+	}
+	lastIndex = index;
+	return index;
 }
-
-const resetAll = () => {
-	//reset timer
-	displayCountDown(initialTimerSecs);
-
-    //hide moles
-    hideMoles()
-	// reset score
-    initialScore = 0;
-	displayScore(initialScore);
-
-};
+function randomTimer(maximum = 1000, minimum = 100) {
+	const popUpTime = Math.round(Math.random() * (maximum - minimum) + minimum);
+	return popUpTime;
+}
+// setting the play sound
+function playPop() {
+	const audio = new Audio("pop.mp3");
+	audio.currentTime = 0;
+	audio.play();
+}
+function whack(event) {
+	// checking the bot    if (!event.isTrusted) return;
+	score.textContent++;
+	playPop();
+	this.parentNode.classList.remove("up");
+}
+function startGame() {
+	holes.forEach((hole) => hole.classList.remove("up"));
+	score.textContent = 0;
+	timer.textContent = 10;
+	timeOver = false;
+	clearInterval(countDown_timer);
+	clearTimeout(popUp_timer);
+	clearTimeout(gameTimer);
+	popUp();
+	countDown();
+	gameTimer = setTimeout(() => (timeOver = true), 10000);
+}
+moles.forEach((mole) => mole.addEventListener("click", whack));
+button.addEventListener("click", startGame);
